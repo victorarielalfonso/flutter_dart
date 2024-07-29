ac869fd8d4a583414bc7464f974977ffb9c1fd71
milimartinetti <milimartinetti@gmail.com>
2024-07-29 15:29:45 -0400

    Prueba nombre

--- main.dart
+++ main.dart
@@ -1,22 +1,8 @@
 import 'dart:io';
 
 void main() {
+  var nombre="Viutor";
+  print(nombre);
   
-  stdout.write('Introduce el primer número: ');
-  var numberOneInput = stdin.readLineSync();
-  var numberOne = int.parse(numberOneInput!); 
- 
-  stdout.write('Introduce el segundo número: ');
-  var numberTwoInput = stdin.readLineSync();
-  var numberTwo = int.parse(numberTwoInput!);
-
-  stdout.write('Introduce Tercer número: ');
-  var numberThreenput = stdin.readLineSync();
-  var numberThree = int.parse(numberThreenput!);
-
-  if(numberOne > numberTwo){ print('El primer número es mayor');
-  }else if(numberOne < numberThree) { print('El tercer número es mayor'); 
-
-  }else { print('El segundo número es mayor');
-  }
+  
 }
