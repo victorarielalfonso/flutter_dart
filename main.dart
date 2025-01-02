04c9a9593c9b655c64fb3b65465433ed8e6c0621
terrynchy <terrynchy@gmail.com>
2025-01-02 15:14:20 -0300

    Agregar class

--- main.dart
+++ main.dart
@@ -1,16 +1,27 @@
 import 'dart:convert';
+import 'dart:ffi';
 import 'dart:io';
 
 void main() {
 
+var persona = pet("Victor", 25);
+var animal = dog("Terry", 9);
+var mothe = mother("Maria", 60);
+print('''El se llama ${persona.name} y tiene la edad de ${persona.age} años y su mascota se llama ${animal.name} con ${animal.age} años.
+Su madre de llama ${mothe.name} y está por cumplir ${mothe.age} años''' ); 
 
-var valor = 9;
-for(var i =1; i <=10; i++){
-var x = i * valor;
-print('${valor}*${i}=${x}');
+  }
+class pet { 
+  pet (this.name, this.age){
+  }
+       late final String name;
+       late final int age;
 }
-
-
-
-
+class dog extends pet {
+  dog(String name, int age) :super(name, age);
 }
+class mother extends pet {
+  mother(String name, int age) :super(name, age);
+}
+  
+  
